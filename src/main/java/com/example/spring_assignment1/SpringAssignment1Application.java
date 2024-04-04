87576b14061db60c22a619bd7be40399357e9b60
Zntlokwana <202249971@ufh.ac.za>
2024-04-05 00:22:37 +0200

    first commit

---
+++ src/main/java/com/example/spring_assignment1/SpringAssignment1Application.java
@@ -0,0 +1,37 @@
+package com.example.spring_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
+@SpringBootApplication
+@RestController
+    public class SpringAssignment1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAssignment1Application.class, args);
+    }
+
+        @GetMapping("/foundation-courses")
+        public List<String> getFoundationCourses() {
+            return Arrays.asList("CSC 111F", "CSC 121F"); // Replace with actual foundation courses
+        }
+
+        @GetMapping("/undergraduate-courses")
+        public List<String> getUndergraduateCourses() {
+            return Arrays.asList("CSC 211", "CSC 213", "CSC 224"); // Replace with actual undergraduate courses
+        }
+
+        @GetMapping("/honors-courses")
+        public List<String> getHonorsCourses() {
+            return Arrays.asList("CSC 511", "CSC 512", "CSC 522", "CSC 524"); // Replace with actual honors courses
+        }
+
+    }
+
+
